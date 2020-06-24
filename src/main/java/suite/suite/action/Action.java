bbed0d40beaf27ebd9d13c270245bc9c73519b94
LPO <pomluk1996@gmail.com>
2020-06-24 23:21:52 +0200

    Action wrap

--- src/main/java/suite/suite/action/Action.java
+++ src/main/java/suite/suite/action/Action.java
@@ -3,6 +3,9 @@ package suite.suite.action;
 import suite.suite.Subject;
 import suite.suite.Suite;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 @FunctionalInterface
 public interface Action {
 
@@ -18,6 +21,18 @@ public interface Action {
 
     default Subject gamble(Subject in) throws Exception {
         return play(in);
+    }
+
+    static<T, U> Action wrap(Function<T, U> function) {
+        return s -> Suite.set(function.apply(s.asExpected()));
+    }
+
+    static<T, U> Action wrap(Class<T> argType, Function<T, U> function) {
+        return s -> Suite.set(function.apply(s.asExpected()));
+    }
+
+    static<T1, T2, U> Action wrap(Class<T1> arg1Type, Class<T2> arg2Type, BiFunction<T1, T2, U> function) {
+        return s -> Suite.set(function.apply(s.asExpected(), s.getAt(1).asExpected()));
     }
 
 }
