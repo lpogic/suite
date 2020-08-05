6a1aecac613cad444360e8c20c3ac8dc8764b542
LPO <pomluk1996@gmail.com>
2020-08-05 22:40:56 +0200

    union i export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,5 @@ open module suite.main {
     exports suite.suite;
     exports suite.suite.action;
     exports suite.suite.util;
+    exports suite.sets;
 }
--- src/main/java/suite/sets/Sets.java
+++ src/main/java/suite/sets/Sets.java
@@ -1,13 +1,18 @@
 package suite.sets;
 
 import suite.suite.Subject;
+import suite.suite.Suite;
 
 public class Sets {
 
-    public static boolean contains(Subject s1, Subject s2) {
-        for(var it : s2.front().keys()) {
-            if(s1.get(it).desolated()) return false;
+    public static boolean contains(Subject major, Subject minor) {
+        for(var it : minor.front().keys()) {
+            if(major.get(it).desolated()) return false;
         }
         return true;
+    }
+
+    public static Subject union(Subject major, Subject minor) {
+        return Suite.insetAll(major.front()).inputAll(minor.front());
     }
 }
