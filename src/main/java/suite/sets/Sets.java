ca583825e8afb3f6652320cac86603526b1c16dc
LPO <pomluk1996@gmail.com>
2020-08-12 21:45:58 +0200

    insec

--- src/main/java/suite/sets/Sets.java
+++ src/main/java/suite/sets/Sets.java
@@ -2,6 +2,9 @@ package suite.sets;
 
 import suite.suite.Subject;
 import suite.suite.Suite;
+import suite.suite.util.FluidIterator;
+
+import java.util.Iterator;
 
 public class Sets {
 
@@ -14,5 +17,16 @@ public class Sets {
 
     public static Subject union(Subject major, Subject minor) {
         return Suite.insetAll(major.front()).inputAll(minor.front());
+    }
+    
+    public static Subject insec(Subject major, Subject minor) {
+        Subject sub = Suite.set();
+        for(var o : minor.front().keys()) {
+            Subject s1 = major.get(o);
+            if(s1.settled()) {
+                sub.set(s1.key().direct(), s1.direct());
+            }
+        }
+        return sub;
     }
 }
