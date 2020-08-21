bc820b4622783d3443725b4de3163ae3e24566fc
LPO <pomluk1996@gmail.com>
2020-08-22 00:25:41 +0200

    fluid in sets

--- src/main/java/suite/sets/Sets.java
+++ src/main/java/suite/sets/Sets.java
@@ -2,26 +2,27 @@ package suite.sets;
 
 import suite.suite.Subject;
 import suite.suite.Suite;
+import suite.suite.util.Fluid;
 
 public class Sets {
 
-    public static boolean contains(Subject major, Subject minor) {
-        for(var it : minor.front().keys()) {
+    public static boolean contains(Subject major, Fluid minor) {
+        for(var it : minor.keys()) {
             if(major.get(it).desolated()) return false;
         }
         return true;
     }
 
-    public static Subject union(Subject major, Subject minor) {
-        return Suite.insetAll(major.front()).inputAll(minor.front());
+    public static Subject union(Fluid major, Fluid minor) {
+        return Suite.insetAll(major).inputAll(minor);
     }
     
-    public static Subject insec(Subject major, Subject minor) {
+    public static Subject insec(Subject major, Fluid minor) {
         Subject sub = Suite.set();
-        for(var o : minor.front().keys()) {
-            Subject s1 = major.get(o);
-            if(s1.settled()) {
-                sub.set(s1.key().direct(), s1.direct());
+        for(var o : minor.keys()) {
+            Subject s = major.get(o);
+            if(s.settled()) {
+                sub.set(s.key().direct(), s.direct());
             }
         }
         return sub;
