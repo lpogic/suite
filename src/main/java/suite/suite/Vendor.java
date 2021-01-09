ecaaa3445a12c89085d63fb9e5137f31893889c4
LPO <pomluk1996@gmail.com>
2021-01-09 19:48:33 +0100

    vendor update

--- src/main/java/suite/suite/Vendor.java
+++ src/main/java/suite/suite/Vendor.java
@@ -1,6 +1,18 @@
 package suite.suite;
 
+import suite.suite.util.Browser;
+import suite.suite.util.Glass;
+
+import java.util.Iterator;
+import java.util.function.Supplier;
+
 public abstract class Vendor extends Subject {
+
+    final Subject $sub;
+
+    public Vendor(Subject $sub) {
+        this.$sub = $sub;
+    }
 
     @Override
     protected Subject materialize() {
@@ -9,6 +21,139 @@ public abstract class Vendor extends Subject {
     @Override
     protected Subject materialize(Object element) {
         throw new UnsupportedOperationException("Vendor is immutable");
+    }
+
+    protected abstract Subject wrap(Subject $);
+
+    protected abstract Subject factor(Subject $);
+
+    protected abstract Subject factor(Subject $, Class<?> type);
+
+    @Override
+    protected Subject jump() {
+        return wrap($sub.jump());
+    }
+
+    @Override
+    protected Subject jump(Object o) {
+        return wrap($sub.jump(o));
+    }
+
+    @Override
+    protected boolean real(Object element) {
+        return $sub.real(element);
+    }
+
+    @Override
+    public Sub in() {
+        return Sub.of(this).in();
+    }
+
+    @Override
+    public Sub in(Object o) {
+        return Sub.of(this).in(o);
+    }
+
+    @Override
+    public Subject getFirst() {
+        return wrap($sub.getFirst());
+    }
+
+    @Override
+    public Subject getLast() {
+        return wrap($sub.getLast());
+    }
+
+    @Override
+    public Subject get(Object o) {
+        return wrap($sub.get(o));
+    }
+
+    @Override
+    public Object direct() {
+        return factor($sub).direct();
+    }
+
+    @Override
+    public <B> B asExpected() {
+        return factor($sub).asExpected();
+    }
+
+    @Override
+    public <B> B as(Class<B> aClass) {
+        return factor($sub, aClass).as(aClass);
+    }
+
+    @Override
+    public <B> B as(Glass<? super B, B> glass) {
+        return factor($sub, glass.getMainClass()).as(glass);
+    }
+
+    @Override
+    public <B> B as(Class<B> aClass, B b) {
+        return factor($sub, aClass).as(aClass, b);
+    }
+
+    @Override
+    public <B> B as(Glass<? super B, B> glass, B b) {
+        return factor($sub, glass.getMainClass()).as(glass, b);
+    }
+
+    @Override
+    public <B> B orGiven(B b) {
+        return factor($sub).orGiven(b);
+    }
+
+    @Override
+    public <B> B orDo(Supplier<B> supplier) {
+        return factor($sub).orDo(supplier);
+    }
+
+    @Override
+    public boolean is(Class<?> aClass) {
+        return factor($sub).is(aClass);
+    }
+
+    @Override
+    public boolean present() {
+        return factor($sub).present();
+    }
+
+    @Override
+    public boolean present(Object o) {
+        return get(o).present();
+    }
+
+    @Override
+    public boolean absent() {
+        return factor($sub).absent();
+    }
+
+    @Override
+    public boolean absent(Object o) {
+        return get(o).absent();
+    }
+
+    @Override
+    public int size() {
+        return $sub.size();
+    }
+
+    @Override
+    public Browser<Subject> iterator(boolean b) {
+        return new Browser<>() {
+            final Iterator<Subject> source = $sub.iterator(b);
+
+            @Override
+            public boolean hasNext() {
+                return source.hasNext();
+            }
+
+            @Override
+            public Subject next() {
+                return wrap(source.next());
+            }
+        };
     }
 
     @Override
